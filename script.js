501977b99cd149f5251c9de4891c7fd6b52b2a7a
Ravi Sharma <sraa7877@gmail.com>
2023-08-16 00:27:20 +0530

    completed

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ const menuButton = document.querySelector('.trigger'),
 
 
 
-const submenu = document.querySelectorAll('.has-child')
+const submenu = document.querySelectorAll('.has-child .icon-small')
 submenu.forEach((menu) => menu.addEventListener('click', toggle))
 
 function toggle(e) {
@@ -43,6 +43,17 @@ function toggle(e) {
     submenu.forEach((item) => item != this ? item.closest('.has-child').classList.remove('expand') : null);
     if (this.closest('.has-child').classList != 'expand');
     this.closest('.has-child').classList.toggle('expand')
+}
+
+
+var stocks = document.querySelectorAll('.products .stock')
+for(let i=0;i<stocks.length;i++){
+  let stock = stocks[i].dataset.stock,
+  available = stocks[i].querySelector('.qty-available').innerHTML,
+  sold = stocks[i].querySelector('.qty-sold').innerHTML,
+  percent = sold*100/stock;
+
+  stocks[i].querySelector('.available').style.width = percent+ '%';
 }
 
 
@@ -71,3 +82,72 @@ searchButton.addEventListener('click',()=>[
 tClose.addEventListener('click',()=>[
   showClass.classList.remove('showsearch')
 ])
+
+
+
+const dptButton = document.querySelector('.dpt-cat .dpt-trigger'),
+      dptClass = document.querySelector('.site')
+
+dptButton.addEventListener('click', ()=>{
+  dptClass.classList.toggle('showdpt')
+})
+
+
+
+
+var productThumb = new Swiper('.small-image', {
+  
+  loop: true,
+  spaceBetween: 10,
+  slidesPerView: 3,
+  freeMode: true,
+  watchSlidesProgress: true,
+  breakpoints: {
+    481: {
+      spaceBetween: 32,
+    }
+  }
+});
+// console.log("yes")
+var productBig = new Swiper('.big-image', {
+  loop: true,
+  autoHeight: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  thumbs: {
+    swiper : productThumb,
+  }
+});
+
+
+
+
+
+
+
+
+const divtoshow = '.mini-cart'
+const divpopup = document.querySelector(divtoshow);
+const divTrigger = document.querySelector('.cart-trigger')
+divTrigger.addEventListener('click',()=>{
+  setTimeout(()=>{
+    if(!divpopup.classList.contains('show')){
+      divpopup.classList.add('show')
+    }
+  },250)
+})
+document.addEventListener('click',(e)=>{
+  const isClosest = e.target.closest(divtoshow)
+  if(!isClosest && divpopup.classList.contains('show')){
+    divpopup.classList.remove('show')
+  }
+})
+
+// window.onload = () => {
+//   document.querySelector('.site').classList.toggle('showmodal')
+// }
+// document.querySelector('.modalclose').addEventListener('click',()=>{
+//   document.querySelector('.site').classList.remove('showmodal')
+// })
